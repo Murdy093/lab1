7704c4e00761788421f0bd1ca7d33346ad65ca83
Murdy093 <qwerty2018228@gmail.com>
2024-10-13 14:15:08 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("Enter the length of the first katet: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the length of the second katet: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        
+        double c = Math.Sqrt(a * a + b * b);
+        double area = (a * b) / 2;
+
+        
+        Console.WriteLine("Hypotenuse: " + c);
+        Console.WriteLine("Area: " + area);
+
+
+        Console.WriteLine("Press any key to complete...");
+        Console.ReadKey();
+    }
+}
